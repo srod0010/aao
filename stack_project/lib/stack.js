554226855d4053fdfc1700fcc5899d5fb80b9af2
Savio Rodrigues <saviofrod@gmail.com>
2019-03-26 17:20:09 -0700

    add constructors

--- stack_project/lib/stack.js
+++ stack_project/lib/stack.js
@@ -21,11 +21,18 @@
 // -----------
 
 class Node {
-
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
 }
 
 class Stack {
-
+    constructor() {
+        this.top = null;
+        this.bottom = null;
+        this.length = 0;
+    }
 }
 
 exports.Node = Node;
