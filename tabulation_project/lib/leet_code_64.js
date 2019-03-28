6afd1a8a1bfcb65757b80def6debf93a9caf574f
Savio Rodrigues <saviofrod@gmail.com>
2019-03-28 10:59:44 -0700

    complete minPathSum

--- tabulation_project/lib/leet_code_64.js
+++ tabulation_project/lib/leet_code_64.js
@@ -1,6 +1,40 @@
 // Work through this problem on https://leetcode.com/problems/minimum-path-sum/ and use the specs given there.
 // Feel free to use this file for scratch work.
 
-function minPathSum(grid) {
+var minPathSum = function (grid) {
+    let table = new Array(grid.length).fill().map(() => new Array(grid[0].length));
 
-}
+    for (let col = 0; col < grid.length; col++) {
+        for (let row = 0; row < grid[col].length; row++) {
+            let above = col - 1;
+            let left = row - 1;
+            // if left and up are not defined add start el
+            if (!grid[above] && !grid[col][left]) {
+
+                table[col][row] = grid[col][row];
+
+            }
+
+
+            // if left & !up -> add left
+            if (grid[col][left] && !grid[above]) {
+                table[col][row] = grid[col][row] + table[col][left];
+            }
+
+
+            // if up & !left -> add up
+            if (grid[above] && !grid[col][left]) {
+                table[col][row] = grid[col][row] + table[above][row];
+            }
+
+            // if up && left -> take minimum
+            if (grid[above] && grid[col][left]) {
+                let aboveSum = grid[col][row] + table[above][row];
+                let leftSum = grid[col][row] + table[col][left];
+                table[col][row] = Math.min(aboveSum, leftSum);
+            }
+        }
+    }
+
+    return table[table.length - 1][table[0].length - 1];
+};
