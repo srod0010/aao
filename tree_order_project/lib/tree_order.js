972cabd0632edb246f1939da9bd44d2d54058b22
Savio Rodrigues <saviofrod@gmail.com>
2019-04-01 12:41:01 -0700

    Implement inOrder and postOrder

--- tree_order_project/lib/tree_order.js
+++ tree_order_project/lib/tree_order.js
@@ -1,9 +1,16 @@
 function inOrderArray(root) {
+    if (!root) return [];
+    let left = inOrderArray(root.left);
+    let right = inOrderArray(root.right);
+    return left.concat([root.val]).concat(right);
 
 }
 
 function postOrderArray(root) {
-
+    if (!root) return [];
+    let left = postOrderArray(root.left);
+    let right = postOrderArray(root.right);
+    return left.concat(right).concat(root.val);
 }
 
 
